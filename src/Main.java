ca4f1d5e0462309c772593b4b1c55fdb9fb70a77
joshua9154 <44042301+joshua9154@users.noreply.github.com>
2023-04-10 11:20:45 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Server_X_Client a = new Server_X_Client();
+        NetworkClient b = new NetworkClient();
+
+        NetworkClient2 c = new NetworkClient2();
+
+        NetworkClient3 d = new NetworkClient3();
+    }
+}
