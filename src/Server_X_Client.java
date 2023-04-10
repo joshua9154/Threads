ea935ef86f44b0e9882fdca94b2309cd6b606812
joshua9154 <44042301+joshua9154@users.noreply.github.com>
2023-04-10 13:43:44 -0400

    updates

--- src/Server_X_Client.java
+++ src/Server_X_Client.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 
 public class Server_X_Client {
@@ -14,6 +15,7 @@ public class Server_X_Client {
         Socket s=null;
         ServerSocket ss2=null;
         System.out.println("Server Listening......");
+        int counter =0;
         try{
             ss2 = new ServerSocket(4445); // can also use static final PORT_NUM , when defined
 
@@ -23,6 +25,7 @@ public class Server_X_Client {
             System.out.println("Server error");
 
         }
+        ArrayList<ServerThread>  players =new ArrayList<>();
 
         while(true){
             try{
@@ -30,6 +33,12 @@ public class Server_X_Client {
                 System.out.println("connection Established");
                 ServerThread st=new ServerThread(s);
                 st.start();
+                players.add(st);
+                counter++;
+                if(counter ==3) {
+                    st.card(5);
+                }
+                System.out.println(counter);
 
             }
 
@@ -53,6 +62,59 @@ class ServerThread extends Thread{
 
     public ServerThread(Socket s){
         this.s=s;
+    }
+
+    public void card (int card){
+
+        try{
+            is= new BufferedReader(new InputStreamReader(s.getInputStream()));
+            os=new PrintWriter(s.getOutputStream());
+
+        }catch(IOException e){
+            System.out.println("IO error in server thread");
+        }
+
+        try {
+            line=is.readLine();
+            while(line.compareTo("QUIT")!=0){
+                line = "Spades "+card;
+                os.println(line);
+                os.flush();
+                System.out.println("Response to Client  :  "+line);
+                line=is.readLine();
+            }
+        } catch (IOException e) {
+
+            line=this.getName(); //reused String line for getting thread name
+            System.out.println("IO Error/ Client "+line+" terminated abruptly");
+        }
+        catch(NullPointerException e){
+            line=this.getName(); //reused String line for getting thread name
+            System.out.println("Client "+line+" Closed");
+        }
+
+        finally{
+            try{
+                System.out.println("Connection Closing..");
+                if (is!=null){
+                    is.close();
+                    System.out.println(" Socket Input Stream Closed");
+                }
+
+                if(os!=null){
+                    os.close();
+                    System.out.println("Socket Out Closed");
+                }
+                if (s!=null){
+                    s.close();
+                    System.out.println("Socket Closed");
+                }
+
+            }
+            catch(IOException ie){
+                System.out.println("Socket Close Error");
+            }
+        }//end finally
     }
 
     public void run() {
